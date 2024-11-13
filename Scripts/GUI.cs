61445b7c9b73336bb8caff92276a919140117292
TYss00 <89635061+TYss00@users.noreply.github.com>
2024-11-13 15:03:28 +0900

    1.0

---
+++ Scripts/GUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUI : MonoBehaviour
+{
+    public void AppQuit()
+    {
+        Debug.Log("³ª°©´Ï´Ù.");
+        Application.Quit();
+    }
+}
